77f0a2995876c678ba6dca2f3468e52ec0b4e3ed
Matt Slater <github@mslater.fastmail.com>
2023-03-17 11:34:11 +0000

    initial commit

---
+++ src/MainView.hx
@@ -0,0 +1,37 @@
+package ;
+
+import haxe.ui.data.DataSource;
+import haxe.ui.events.UIEvent;
+import haxe.ui.containers.VBox;
+import haxe.ui.events.MouseEvent;
+
+@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
+class MainView extends VBox {
+    public function new() {
+        super();
+    }
+
+    @:bind(testDropdown, UIEvent.CHANGE)
+    private function testDropdown_onChange(e:haxe.ui.events.UIEvent) {
+        indexLabel.text = Std.string(testDropdown.selectedIndex);
+    }
+    
+    @:bind(indexButton, MouseEvent.CLICK)
+    private function indexButton_onClick(e:haxe.ui.events.UIEvent) {
+        testDropdown.selectedIndex = 0;
+    }
+
+    private function replaceButton_onClick(e:haxe.ui.events.UIEvent) {
+        var newSource = new DataSource();
+        newSource.add("New item #1");
+        newSource.add("New item #2");
+        newSource.add("New item #3");
+        newSource.add("New item #4");
+        newSource.add("New item #5");
+        testDropdown.selectedIndex = -1;
+        testDropdown.dataSource = newSource;
+        testDropdown.selectedIndex = 0;
+    }
+
+
+}
